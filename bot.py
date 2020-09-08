2c3efc0a37e5534332c258b54a5801702d7759b3
Keanu Ghorbanian <kghorban@cern.sh>
2020-09-08 13:36:48 +0100

    reply-response dictionary is better practice than what I was doing before

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
-from fbchat import log, Client, MessageReaction
+from fbchat import log, Client
 from fbchat.models import *
 from dotenv import load_dotenv
 from bot_functions import *
-import string
 import os
 import random
 
@@ -10,31 +9,53 @@ load_dotenv()
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASWD')
 
+# dictionaries of reply-responses
+quick_replies = {
+    "bot speak": "woof!",
+    "good bot": "🥰",
+    "sexy bot": "😉",
+}
+react_replies = {
+    "good bot": MessageReaction.HEART,
+    "bad bot": MessageReaction.SAD,
+}
+
 
 class BarelyKnowHerBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         # just look at text messages
         if message_object.text is not None:
             message = message_object.text
-            message_stripped = message.lower().translate(str.maketrans('', '', string.punctuation))
+            message_stripped = clean_message(message)
 
-            if message_stripped == "bot speak":
-                self.send(Message(text="woof!"), thread_id=thread_id, thread_type=thread_type)
-            elif message_stripped == "good bot":
-                self.reactToMessage(message_id=message_object.uid, reaction=MessageReaction.HEART)
+            funny_word = good_word(message_stripped)
+
+            # simple reply-responses
+            if message_stripped in quick_replies:
+                self.send(Message(text=quick_replies[message_stripped], reply_to_id=message_object.uid),
+                          thread_id=thread_id, thread_type=thread_type)
+            if message_stripped in react_replies:
+                self.reactToMessage(message_id=message_object.uid, reaction=react_replies[message_stripped])
+
+            # bot appreciates a good joke (but doesn't laugh at its own jokes)
+            elif 'i hardly know her' in message_stripped and author_id != self.uid:
+                self.reactToMessage(message_id=message_object.uid, reaction=MessageReaction.SMILE)
 
             # absolutely hilarious joke
             # reply if not replying to self, and passing a roll
-            reply = good_word(message)
-            if reply \
+            elif funny_word \
                     and author_id != self.uid \
-                    and random.random() < 0.2:
+                    and random.random() < .2:
                 log.info(f"The message: \n{message}\n contains a hilarious word. Will reply now!")
 
-                funny_quip = f'{reply}? I hardly know her!'
+                funny_quip = f'{funny_word}? I hardly know her!'
                 self.send(Message(text=funny_quip, reply_to_id=message_object.uid), thread_id=thread_id,
                           thread_type=thread_type)
+
             else:
+                # special surprise
+                if random.random() < .001:
+                    self.send(Message(text='𓀐𓂸'), thread_id=thread_id, thread_type=thread_type)
                 # Sends the data to the inherited onMessage, so that we can still see when a message is received
                 super(BarelyKnowHerBot, self).onMessage(
                     author_id=author_id,
@@ -47,4 +68,3 @@ class BarelyKnowHerBot(Client):
 
 client = BarelyKnowHerBot(EMAIL, PASSWORD)
 client.listen()
-
--- bot_functions.py
+++ bot_functions.py
@@ -1,14 +1,18 @@
+import re
 from nltk.tokenize import word_tokenize
 import pyphen
 dic = pyphen.Pyphen(lang='en_GB')
 
 
-# if message contains a funny word, return that word if it has more than one syllable. otherwise return None
 def good_word(text: str):
+    """
+    if message contains a funny word, return that word (the last word if multiple) if it has more than one syllable.
+    Otherwise return None
+    """
     words = [word.lower() for word in word_tokenize(text)]
     for word in reversed(words):
         if len(word) > 25:
-            pass
+            continue
         if word[-1] == 'a' \
                 or word[-2:] in ('er', 'or', 'ar', 're') \
                 or word[-3:] == 'eur':
@@ -16,3 +20,10 @@ def good_word(text: str):
             if len(dic.inserted(word).split('-')) > 1:
                 return word.lower()
     return None
+
+
+def clean_message(text: str):
+    """
+    return lowercase string stripped of trailing whitespaces and nonalphanumeric characters
+    """
+    return re.sub(r'[^\sa-zA-Z0-9]', '', text).lower().strip()
